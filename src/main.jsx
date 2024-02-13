7e267d42d8092c6af2329fa32701a304b86cce03
Shivam Shende <Shivamshende200@gmail.com>
2024-02-13 12:38:46 +0530

    refactor - update redirect uri

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     domain="dev-eer2erp88huqadhk.us.auth0.com"
     clientId="lukH5FRjA45lCt0h6LOW2yV3rLdvD8tH"
     authorizationParams={{
-      redirect_uri: window.location.origin
+      redirect_uri: "https://shivamshende.github.io"
     }}
   >
     <Profile />
