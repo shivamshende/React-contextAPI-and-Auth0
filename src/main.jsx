d4c3f7020dfc191e7f0895b0ac69bdca4e44a0e2
Shivam Shende <Shivamshende200@gmail.com>
2024-02-13 13:14:58 +0530

    refactor - redirect uri

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import Profile from './Profile';
 import LoginLogout from './LoginLogout';
 import './App.css';
 
+// Determine the base URL based on the environment
+const baseUrl = process.env.NODE_ENV === 'production' ? "https://shivamshende.github.io" : "http://localhost:5173";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Auth0Provider
     domain="dev-eer2erp88huqadhk.us.auth0.com"
     clientId="lukH5FRjA45lCt0h6LOW2yV3rLdvD8tH"
     authorizationParams={{
-      redirect_uri: "https://shivamshende.github.io"
+      redirect_uri: baseUrl
     }}
   >
     <Profile />
